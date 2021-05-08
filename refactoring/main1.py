9bd78ea6ecf4ecafa64a5c08a72e6357bddfb854
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2021-05-08 10:56:46 +0200

    Excercise Refactoring Winc
    Made the main code cleaner.(main = from winc) main1 = mine

---
+++ refactoring/main1.py
@@ -0,0 +1,88 @@
+__winc_id__ = '9920545368b24a06babf1b57cee44171'
+__human_name__ = 'refactoring'
+
+class electrician:
+    def __init__(self, name, price):
+        self.name = name
+        if "electrician" in specialists:
+            specialists["electrician"].append({"name": name, "price": price})
+        else:
+            specialists["electrician"] = [{"name": name, "price": price}]
+
+
+class painter:
+    def __init__(self, name, price):
+        self.name = name
+        if "painter" in specialists:
+            specialists["painter"].append({"name": name, "price": price})
+        else:
+            specialists["painter"] = [{"name": name, "price": price}]
+
+
+class plumber:
+    def __init__(self, name, price):
+        self.name = name
+        if "plumber" in specialists:
+            specialists["plumber"].append({"name": name, "price": price})
+        else:
+            specialists["plumber"] = [{"name": name, "price": price}]
+
+
+def get_cheapest_specialist(needed_people):
+    indexPreferred = -1
+    min_price = 0
+    for professional_person in needed_people:
+        if indexPreferred == -1:
+            indexPreferred = 0
+            min_price = professional_person['price']
+        else:
+            if professional_person['price'] < min_price:
+                indexPreferred = needed_people.index(professional_person)
+    return
+
+
+class Homeowner:
+    def __init__(self, name, address, needs, specialists):
+        self.name = name
+        self.address = address
+        self.needs = needs
+        self.specialists = specialists
+
+    def make_contracts(self):
+        contracts = []
+        print("specialists :"+specialists.__repr__())
+        print(specialists['electrician'])
+
+        for need in self.needs:
+            if need in self.specialists:
+                needed_people = self.specialists[need]
+                if len(needed_people) == 1:
+                    contracts.append(needed_people[0]['name'])
+                elif len(needed_people) == 0:
+                    print(f"Voor het beroep {need} is nog niemand opgevoerd")
+                else:
+                    desired_specialist = get_cheapest_specialist(needed_people)
+                    contracts.append(desired_specialist['name'])
+            else:
+                print(f"there's no {need}")
+        return contracts
+
+
+specialists = {}
+
+alice = electrician('Alice Aliceville', 0)
+bob = painter('Bob Bobsville', 0)
+craig = plumber('Craig Craigsville', 0)
+
+alfred = Homeowner('Alfred Alfredson', 'Alfredslane 123',
+                   ['painter', 'plumber'], specialists)
+for contract in alfred.make_contracts():
+    print("alfred needs persons : "+contract)
+
+bert = Homeowner('Bert Bertson', 'Bertlane 231', ['plumber'], specialists)
+for contract in bert.make_contracts():
+    print("bert needs persons: "+contract)
+
+candice = Homeowner('Candice Candicedottir', 'Candicelane 432', ['electrician', 'painter'], specialists)
+for contract in candice.make_contracts():
+    print("candince needs persons: "+ contract)
